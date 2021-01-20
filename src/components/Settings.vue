685a9bdfb344fa4c3b16d9ddd6b78fff35c8f0e2
Doniyor <doniyor199696@gmail.com>
2021-01-20 16:22:41 +0600

    added sorting

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="locations">
-    <div
-      class="location d-flex justify-content-between my-2"
-      v-for="location in locations"
-      :key="location.id"
+    <Draggable
+      :list="locations"
+      :disabled="!enabled"
+      class="list-group"
+      ghost-class="ghost"
+      @start="dragging = true"
+      @end="dragging = false"
     >
-      <div>
-        <HamburgerIcon class="icon big move-cursor" />
-        <span>{{ location.name }}</span>
+      <div
+        class="location d-flex justify-content-between my-2"
+        v-for="location in locations"
+        :key="location.id"
+      >
+        <div>
+          <HamburgerIcon class="icon big move-cursor handle" />
+          <span>{{ location.name }}</span>
+        </div>
+        <div>
+          <DeleteIcon class="icon big" @click="deleteLocation(location.id)" />
+        </div>
       </div>
-      <div>
-        <DeleteIcon class="icon big" @click="deleteLocation(location.id)" />
-      </div>
-    </div>
+    </Draggable>
     <AddLocation />
   </div>
 </template>
@@ -23,10 +32,17 @@ import { mapActions, mapGetters } from "vuex";
 import HamburgerIcon from "@/assets/hamburger.svg";
 import DeleteIcon from "@/assets/delete.svg";
 import AddLocation from "@/components/AddLocation.vue";
+import Draggable from "vuedraggable";
 
 export default Vue.extend({
   name: "Settings",
-  components: { AddLocation, HamburgerIcon, DeleteIcon },
+  components: { AddLocation, HamburgerIcon, DeleteIcon, Draggable },
+  data() {
+    return {
+      enabled: true,
+      dragging: false
+    };
+  },
   computed: {
     ...mapGetters(["locations"])
   },
